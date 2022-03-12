9f1f3243e81b8571583f816d733bdd360c2c21e7
sliptonic <shopinthewoods@gmail.com>
2022-03-12 15:50:11 -0600

    Working on simulation
    working on camotics with multiple file output and tests

--- src/Mod/Path/PathScripts/PathJob.py
+++ src/Mod/Path/PathScripts/PathJob.py
@@ -671,6 +671,8 @@ class ObjectJob:
         if getattr(obj, "Operations", None):
             # obj.Path = obj.Operations.Path
             self.getCycleTime()
+            if hasattr(obj, "PathChanged"):
+                obj.PathChanged = True
 
     def getCycleTime(self):
         seconds = 0
